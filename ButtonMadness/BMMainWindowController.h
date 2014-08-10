79175f7ae2a38123f5db57c85f039d67b6509cc5
Tong Guo <Tong-G@outlook.com>
2014-08-10 23:24:05 +0800

    Routine backup.

--- ButtonMadness/BMMainWindowController.h
+++ ButtonMadness/BMMainWindowController.h
@@ -36,6 +36,34 @@
 // BMMainWindowController class
 @interface BMMainWindowController : NSWindowController
 
+#pragma mark Pop Up Box
+@property ( assign ) IBOutlet NSBox* _popUpBox;
+    // Nib based controls
+    @property ( assign ) IBOutlet NSPopUpButton* _nibBasedPopUpDown;
+    @property ( assign ) IBOutlet NSPopUpButton* _nibBasedPopUpRight;
+        @property ( assign ) IBOutlet NSMenu* _nibBasedPopUpRightMenu;
+
+    // Code based controls
+    @property ( assign ) IBOutlet NSView* _popUpBoxPlaceHolderUp;
+        @property ( retain ) NSPopUpButton* _codeBasedPopUpDown;
+
+    @property ( assign ) IBOutlet NSView* _popUpBoxPlaceHolderDown;
+        @property ( retain ) NSPopUpButton* _codeBasedPopUpRight;
+
+#pragma mark Button Box
+@property ( assign ) IBOutlet NSBox* _buttonBox;
+    // Nib based controls
+    @property ( assign ) IBOutlet NSButton* _nibBasedBevelButton;
+    @property ( assign ) IBOutlet NSButton* _nibBasedSquareButton;
+
+    // Code based controls
+    @property ( assign ) IBOutlet NSView* _buttonBoxPlaceHolderUp;
+        @property ( retain ) NSButton* _codeBasedBevelButton;
+    @property ( assign ) IBOutlet NSView* _buttonBoxPlaceHolderDown;
+        @property ( retain ) NSButton* _codeBasedSquareButton;
+
+#pragma mark Segmented 
+
 + ( id ) mainWindowController;
 
 @end // BMMainWindowController
