62d77d9b16b81f3513f3fbb399666c9c60cf04ed
Tong Guo <Tong-G@outlook.com>
2014-08-10 23:46:15 +0800

    Complete the connections between controls and nib file.

--- ButtonMadness/BMMainWindowController.h
+++ ButtonMadness/BMMainWindowController.h
@@ -36,33 +36,58 @@
 // BMMainWindowController class
 @interface BMMainWindowController : NSWindowController
 
-#pragma mark Pop Up Box
-@property ( assign ) IBOutlet NSBox* _popUpBox;
+#pragma mark Pop Up Buttons Box
+@property ( assign ) IBOutlet NSBox* _popUpButtonsBox;
     // Nib based controls
-    @property ( assign ) IBOutlet NSPopUpButton* _nibBasedPopUpDown;
-    @property ( assign ) IBOutlet NSPopUpButton* _nibBasedPopUpRight;
-        @property ( assign ) IBOutlet NSMenu* _nibBasedPopUpRightMenu;
+    @property ( assign ) IBOutlet NSPopUpButton* _nibBasedPopUpButtonDown;
+    @property ( assign ) IBOutlet NSPopUpButton* _nibBasedPopUpButtonRight;
+        @property ( assign ) IBOutlet NSMenu* _nibBasedPopUpButtonRightMenu;
 
     // Code based controls
-    @property ( assign ) IBOutlet NSView* _popUpBoxPlaceHolderUp;
-        @property ( retain ) NSPopUpButton* _codeBasedPopUpDown;
+    @property ( assign ) IBOutlet NSView* _popUpButtonsBoxPlaceholderUp;
+        @property ( retain ) NSPopUpButton* _codeBasedPopUpButtonDown;
 
-    @property ( assign ) IBOutlet NSView* _popUpBoxPlaceHolderDown;
-        @property ( retain ) NSPopUpButton* _codeBasedPopUpRight;
+    @property ( assign ) IBOutlet NSView* _popUpButtonsBoxPlaceholderDown;
+        @property ( retain ) NSPopUpButton* _codeBasedPopUpButtonRight;
 
-#pragma mark Button Box
-@property ( assign ) IBOutlet NSBox* _buttonBox;
+#pragma mark Buttons Box
+@property ( assign ) IBOutlet NSBox* _buttonsBox;
     // Nib based controls
     @property ( assign ) IBOutlet NSButton* _nibBasedBevelButton;
     @property ( assign ) IBOutlet NSButton* _nibBasedSquareButton;
 
     // Code based controls
-    @property ( assign ) IBOutlet NSView* _buttonBoxPlaceHolderUp;
+    @property ( assign ) IBOutlet NSView* _buttonsBoxPlaceholderUp;
         @property ( retain ) NSButton* _codeBasedBevelButton;
-    @property ( assign ) IBOutlet NSView* _buttonBoxPlaceHolderDown;
+    @property ( assign ) IBOutlet NSView* _buttonsBoxPlaceholderDown;
         @property ( retain ) NSButton* _codeBasedSquareButton;
 
-#pragma mark Segmented 
+#pragma mark Segmented Control Box
+@property ( assign ) IBOutlet NSBox* _segmentedControlsBox;
+    // Nib based controls
+    @property ( assign ) IBOutlet NSSegmentedControl* _nibBasedSegmentedControl;
+
+    // Code based control
+    @property ( assign ) IBOutlet NSView* _segmentedControlsBoxPlaceholder;
+        @property ( retain ) NSSegmentedControl* _codeBasedSegmentedControl;
+
+#pragma mark Matrixes Box
+@property ( assign ) IBOutlet NSBox* _matrixesBox;
+    // Nib based controls
+    @property ( assign ) IBOutlet NSMatrix* _nibBasedMatrix;
+
+    // Code based controls
+    @property ( assign ) IBOutlet NSView* _matrixesBoxPlaceholder;
+        @property ( retain ) NSMatrix* _codeBasedMatrix;
+
+#pragma mark Level Indicators Box
+@property ( assign ) IBOutlet NSBox* _levelIndicatorsBox;
+    // Nib based controls
+    @property ( assign ) IBOutlet NSLevelIndicator* _nibBasedLevelIndicator;
+
+    // Code based controls
+    @property ( assign ) IBOutlet NSView* _levelIndicatorsPlaceholder;
+        @property ( retain ) NSLevelIndicator* _codeBasedLevelIndicator;
 
 + ( id ) mainWindowController;
 
