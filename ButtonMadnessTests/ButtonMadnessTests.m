3e7d95475e1e23b7cbb6858830b9b37be9727517
Tong Guo <Tong-G@outlook.com>
2014-08-10 16:57:36 +0800

    Initial Commit.

---
+++ ButtonMadnessTests/ButtonMadnessTests.m
@@ -0,0 +1,34 @@
+//
+//  ButtonMadnessTests.m
+//  ButtonMadnessTests
+//
+//  Created by Tong G. on 8/10/14.
+//  Copyright (c) 2014 Tong G. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ButtonMadnessTests : XCTestCase
+
+@end
+
+@implementation ButtonMadnessTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
